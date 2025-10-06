f6ef6f03604ad986204f549ec560738fbd5d6608
Hammam <hammammm657@gmail.com>
2025-10-06 10:37:04 +0700

    optimasi UI

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="icon" href="{{ asset('images/logo.png') }}" type="image/png">
     
     @vite(['resources/scss/app.scss', 'resources/js/app.js'])
     @livewireStyles
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
-        <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid px-4">
             <a class="navbar-brand" href="{{ url('/') }}">
                 <i class="bi bi-book-half"></i> 
                 {{ config('app.name', 'Laravel') }}
@@ -18,7 +19,7 @@
         </div>
     </nav>
 
-    <main class="container py-4">
+    <main class="container-fluid px-4 pt-4">
         @yield('content')
     </main>
 
@@ -30,5 +31,6 @@
 
     @livewireScripts
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    @stack('scripts')
 </body>
 </html>
